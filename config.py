ea3b90023cb98055e5e6502302fb9845ba91cdf7
NEERAJKing11 <neerajking3605@gmail.com>
2026-01-01 22:03:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@
 import os
 
 # Telegram Bot Token (BotFather se lein)
-API_TOKEN = "YOUR_BOT_TOKEN_HERE"
+API_TOKEN = "8516847684:AAGF3_C_EcCeMVM2hEzogmSw2obbyphVaTQ"
 
 # MongoDB Connection String (MongoDB Atlas se lein)
-MONGO_URI = "YOUR_MONGODB_CONNECTION_STRING_HERE"
+MONGO_URI = "mongodb+srv://sardakumari905_db_user:p6yUp70gQGLwYTDN@cluster0.0r1bebe.mongodb.net/?appName=Cluster0"
 
 # Database Details
 DB_NAME = "BoardWallahDB"
 COLLECTION_NAME = "students"
 
 # Group ID (Jahan bot kaam karega)
-GROUP_ID = -1001234567890  # Apne group ka numeric ID dalein
-OWNER_ID = 123456789       # Apana khud ka Telegram ID dalein (Admin commands ke liye)
+GROUP_ID = -1003464518144  # Apne group ka numeric ID dalein
+OWNER_ID = 7866235233       # Apana khud ka Telegram ID dalein (Admin commands ke liye)
